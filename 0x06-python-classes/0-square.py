15fea8b1128949a1c5353a57d825749c954963c9
WaltTersly <terslykifugo96@gmail.com>
2022-06-22 02:01:50 +0000

    empty function

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+@author: Walter chege
+"""
+
+
+class Square:
+    """Empty class Square that defines a square
+    Attributes:
+        empty
+    """
+    pass
