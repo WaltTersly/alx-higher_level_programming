2bd70d20993384b6d24c39341c958b454dd9ef1f
WaltTersly <terslykifugo96@gmail.com>
2022-06-28 07:51:29 +0000

    empyclass

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+@author: Walter chege
+"""
+
+
+class Rectangle:
+    """Empty class Rectangle that defines a rectangle figure
+    Attributes:
+        empty
+    """
+
+    pass
