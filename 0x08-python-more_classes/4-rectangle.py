25feefe525a0a1ebd6f3ed00cb10a9f3d3957493
WaltTersly <terslykifugo96@gmail.com>
2022-06-28 09:58:17 +0000

    Eval is magic

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -78,8 +78,9 @@ class Rectangle:
             if h != self.__height - 1:
                 rect.append("\n")
         return ("".join(rect))
-        
+
     def __repr__(self):
-	"""repr method to enable create new instance using #"""
-        return "Rectangle(" + str(self.__width) +\
-            ", " + str(self.__height) + ")"
+        """Return the string representation of rectangle."""
+        rect = "Rectangle(" + str(self.__width)
+        rect += ", " + str(self.__height) + ")"
+        return rect
