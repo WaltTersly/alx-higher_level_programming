82f1bf4a4e36b0b293a2ac710382df4b3cf3bdda
WaltTersly <terslykifugo96@gmail.com>
2022-09-28 05:34:19 +0300

    Search API

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,39 +1,27 @@
 #!/usr/bin/python3
-"""Takes in a letter and sends a POST request to
-http://0.0.0.0:5000/search_user with the letter as
-a parameter.
-Constraints:
-    The letter must be set in the variable q
-    if no argument is given, set q=""
-    If the response body is properly JSON formatted
-    and not empty, display the id and name like this:
-    [<id>] <name>, Otherwise:
-        Display Not a valid JSON if the JSON is invalid
-        Display No result if the JSON is empty
-    Packages requests and sys should be used
-    No other package should be imported
 """
-
-import requests
+Python script that takes in a letter and sends a POST
+request to http://0.0.0.0:5000/search_user with the letter
+as a parameter.
+"""
 from sys import argv
+import requests
 
 
-def main():
-    """Send POST request to URL"""
-    letter = {'q': ""}
-    if len(argv) == 2:
-        letter['q'] = argv[1]
-    r = requests.post("http://0.0.0.0:5000/search_user", data=letter)
-    try:
-        myjson = r.json()
-    except ValueError:
-        print("Not a valid JSON")
+if __name__ == "__main__":
+    if len(argv) < 2:
+        letter = ""
     else:
-        if myjson == {}:
-            print("No result")
+        letter = argv[1]
+    url = 'http://0.0.0.0:5000/search_user'
+    payload = {'q': letter}
+    r = requests.post(url, data=payload)
+
+    try:
+        dic = r.json()
+        if dic:
+            print("[{}] {}".format(dic.get('id'), dic.get('name')))
         else:
-            print(f"[{myjson.get('id')}] {myjson.get('name')}")
-
-
-if __name__ == '__main__':
-    main()
+            print("No result")
+    except ValueError as e:
+        print("Not a valid JSON")
