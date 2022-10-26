efa222214d973c0c55d5ccdf432fa806b6db7d10
WaltTersly <terslykifugo96@gmail.com>
2022-10-26 10:53:53 +0300

    Starwars movie title

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,14 +1,16 @@
 #!/usr/bin/node
+
+const id = process.argv[2];
+const url = 'https://swapi-api.hbtn.io/api/films/' + id;
 const request = require('request');
-let url = 'http://swapi.co/api/films/';
-let episode = process.argv[2];
-request(url + episode, function (err, response, body) {
+
+request(url, function (err, response, body) {
   if (err) {
     console.log(err);
   } else if (response.statusCode === 200) {
-    let jsonobj = JSON.parse(body);
-    console.log(jsonobj.title);
+    body = JSON.parse(body);
+    console.log(body.title);
   } else {
-    console.log('An error occured. Status code: ' + response.statusCode);
+    console.log('Erorr Code:' + response.statusCode);
   }
 });
