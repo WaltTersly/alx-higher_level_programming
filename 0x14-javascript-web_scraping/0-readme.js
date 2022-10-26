7af5853e975fa4f00104120a6a5c148bb1dca7b9
WaltTersly <terslykifugo96@gmail.com>
2022-10-26 09:03:42 +0300

    Readme.md

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const file = process.argv[2];
+const fs = require("fs");
+fs.readFile(file, 'utf-8', (err, data) => {
+    if (err) {  
+        console.error(err);
+    } else {
+        console.log(data);
+    }
+});
